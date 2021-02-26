131ab5e908cc6af32bcd6cf58b40498a01184485
teamfour01 <79622506+teamfour01@users.noreply.github.com>
2021-02-26 20:09:36 +0530

    Add files via upload

--- application/models/mainmodel.php
+++ application/models/mainmodel.php
@@ -1,72 +1,188 @@
 <?php
 class mainmodel extends CI_model
 {
-	public function regaction($a,$b)
-
+	public function encpswd($pass)
+	{
+		return password_hash($pass, PASSWORD_BCRYPT);
+	}
+	//Insert Registered Details
+	public function inreg($a,$b)
 	{
 		$this->db->insert("login",$b);
-		$userid=$this->db->insert_id();
-		$a["u_id"]=$userid;
-		$this->db->insert("reg",$a);
+		$loginid=$this->db->insert_id();
+		$a['loginid']=$loginid;
+	   $this->db->insert("register",$a);
 	}
 	
-	public function encpaswd($pass)
-
+public function view()
 	{
-		return password_hash($pass,PASSWORD_BCRYPT);
-	
-	}
-
-		public function details()   
-		{
 		$this->db->select('*');
-		$this->db->join('login','login.id=reg.u_id','inner');
-		$qry=$this->db->get("reg");
-		return $qry;
-		}
-		public function approve($id)
-	{
-		$this->db->set('status','1');
-		$qry=$this->db->where('id',$id);
-		$qry=$this->db->update("login");
-		return $qry;
+		$this->db->join('login','login.id=reg.loginid','inner');
+		$qry=$this->db->get("register");
+        return $qry;
 	}
-	public function reject($id)
-	{
-		$this->db->set('status','2');
-		$qry=$this->db->where('id',$id);
-		$qry=$this->db->update("login");
-		return $qry;
-	}
-	public function getuserid($uname)
-		{
-			$this->db->select('id');
-			$this->db->from("login");
-			$this->db->where("username",$uname);
-			return $this->db->get()->row('id');
-		}
-		public function getuser($id)
-		{
-			$this->db->select('*');
-			$this->db->from("login");
-			$this->db->where("id",$id);
-			return $this->db->get()->row();
-		}
-
-			public function verifypass($pass,$qry)
-	{
-		return password_verify($pass,$qry);
-	}
-			public function selectpass($uname,$pass)
+	
+   public function select($email,$pass)
 	{
 		$this->db->select('password');
 		$this->db->from("login");
-		$this->db->where("username",$uname);
+		$this->db->where("email",$email);
 		$qry=$this->db->get()->row('password');
 		return $this->verifypass($pass,$qry);
 	}
+	public function verifypass($pass)
+	   {
+         return password_hash($pass, PASSWORD_BCRYPT);
+          
+        }
+	public function getuserid($email)
+	{
+		$this->db->select('id');
+		$this->db->from("login");
+		$this->db->where("email",$email);
+		return $this->db->get()->row('id');
+	}
+	public function getuser($id)
+	{
+		$this->db->select('*');
+		$this->db->from("login");
+		$this->db->where("id",$id);
+		return $this->db->get()->row();
+	}
 
+// 	//insert Airport Details
+// 	public function airportmodel($a)
+// 	{
+// 		$this->db->insert("airport",$a);
+// 	}
+// 	//insert Airport Details
+// public function airportmodel($a)
+// {
+// $this->db->insert("airport",$a);
+// }
+public function flightdetails($a)
+{
+$this->db->insert("flightdetails",$a);
 }
 
 
+
+//notification
+
+
+//fetch data from flight table to notification adding form
+
+public function dataselect()
+{
+$this->db->select('*');
+$qry=$this->db->get("flightdetails");
+return $qry;
+
+}
+
+//insert notification by admin
+
+public function notification($n)
+{
+$this->db->insert("notification",$n);
+
+}
+
+//delete
+public function deletedate($d)
+{
+$this->db->where('depdate<',$d);
+$this->db->delete("flightdetails");
+}
+
+public function aviewflight()
+{
+$this->db->select('*');
+$qry=$this->db->get("flightdetails");
+return $qry;
+}
+public function uviewflight($dep,$arr,$depdate)
+{
+$this->db->select('*');
+$this->db->where("departure",$dep);
+$this->db->where("arrival",$arr);
+$this->db->where("depdate",$depdate);
+
+$qry=$this->db->get("flightdetails");
+return $qry;
+}
+public function search($state)
+{
+$this->db->select('*');
+$this->db->where("state",$state);
+$qry=$this->db->get("airport");
+return $qry;
+}
+//User view
+public function reguser1()
+{
+$this->db->select('*');
+$this->db->join('login','login.id=register.loginid','inner');
+$qry=$this->db->get("register");
+return $qry;
+}
+//User profile updation
+public function regupdateform($id)
+
+{
+$this->db->select('*');
+$qry=$this->db->join("login",'login.id=register.loginid','inner');
+$qry=$this->db->where("register.loginid",$id);
+$qry=$this->db->get("register");
+return $qry;
+}
+public function regupdateform1($a,$b,$id)
+{
+        $this->db->select('*');
+        $qry=$this->db->where("loginid",$id);
+        $qry=$this->db->join('register','login.id=register.loginid','inner');
+        $qry=$this->db->update("register",$a);
+        $qry=$this->db->where("login.id",$id);
+        $qry=$this->db->update("login",$b);
+        return $qry;
+
+
+}
+//airprt details view
+public function airportdetails1()
+{
+
+$this->db->select('*');
+$qry=$this->db->get("airport");
+return $qry;
+
+}
+//airport delete
+public function deleteairport($id)
+{
+$this->db->select('*');
+$this->db->where('id',$id);
+$this->db->delete("airport");
+}
+//airport update
+public function airportupdateform($id)
+
+{
+$this->db->select('*');
+$qry=$this->db->get("airport");
+return $qry;
+}
+public function airportupdateform1 ($a,$id)
+{
+        $this->db->select('*');
+        $qry=$this->db->where("id",$id);
+        $qry=$this->db->update("airport",$a);
+        return $qry;
+
+
+}
+
+}
+
+	
 ?>
